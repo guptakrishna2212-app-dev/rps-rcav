bea923a4a627a2e86f27124f60fa4e6102c16bac
Krishna Gupta <kgupta6@chicagobooth.edu>
2023-04-10 17:39:37 +0000

    almost_final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,51 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
 
+  def play_rock
+
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render ({ :template => "game_templates/play_rock.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+    render ({ :template => "game_templates/play_paper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+    render ({ :template => "game_templates/play_scissors.html.erb" })
+  end
+
+  def rules
+    render({:template => "game_templates/play_rules.html.erb"})
+  end
 end
